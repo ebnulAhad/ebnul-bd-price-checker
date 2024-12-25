4f9a52fa69815d3de9ca3ac20469436b3a0c3a3e
ebnulAhad <142676349+ebnulAhad@users.noreply.github.com>
2024-12-26 05:38:50 +0600

    changes for mobile

--- app/components/sixth/sixth.tsx
+++ app/components/sixth/sixth.tsx
@@ -3,45 +3,32 @@ import "./sixth.css";
 import Link from "next/link";
 function Sixth() {
   function login() {
-    const firstLine = document.querySelectorAll('.line-1,.line-2, .line-3, .line-4, .circle-s');
-    firstLine.forEach(e => {
-      if (e.classList.contains('inactive')) {
-        e.classList.remove('inactive');
-        e.classList.add('active');
-        e.classList.add('glow');
-      } 
-      else if(e.classList.contains('glow')){
-        e.classList.remove('glow');
-        e.classList.add('inactive');
-        e.classList.remove('active');
-      }      
-
+    const Lines = document.querySelectorAll('.line');
+    Lines.forEach(e => {
+      e.classList.toggle("glow")
     });
   }
 
-  function pop() {
-    const glow = document.querySelector('.circle-sm');
-    glow.classList.toggle('glow');
-  }
+
   
   return (
     <div className="signup">
-      <div onClick={pop} className="circle circle-sm">
+      <div className="circle circle-sm glow">
         <button onClick={login} >Sign up</button>
-        <div className="line line-1 inactive"></div>
+        <div className="line line-1 "></div>
       </div>
 
       <div className="circle-row" >
-        <div className="circle circle-L line line-2 inactive">
+        <div className="circle circle-L line line-2  ">
           <input className="form-input" type="text" placeholder="Username"/>
         </div>
-        <div className="circle circle-L1 line line-3 inactive">
+        <div className="circle circle-L1 line line-3  ">
           <input className="form-input" type="text" placeholder="Password"/>
         </div>
       </div>
 
-      <div className="circle circle-m line line-4 inactive other-line"><Link href={"#"} className="forgot-pass">Forgot Password?</Link></div>
-      <div className="circle circle-s inactive other-line"><Link href={"#"}>Login</Link></div>
+      <div className="circle circle-m line line-4  other-line "><Link href={"#"} className="forgot-pass">Forgot Password?</Link></div>
+      <div className="circle circle-s  other-line line noline line-5"><Link href={"#"}>Login</Link></div>
 
     </div>
   );
